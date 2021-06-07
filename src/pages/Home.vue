471ad25a22680fcb3b2f2f35974f91e268c9a0e5
Willer <willergmpaternostro@gmail.com>
2021-06-07 12:26:56 -0300

    banner novo

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -38,12 +38,12 @@
         >
             <q-carousel-slide :name="1" class="column no-wrap">
                 <div class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap">
-                    <q-img  class="rounded-borders col-12 full-height" src="https://cdn.quasar.dev/img/chicken-salad.jpg"  />
+                    <q-img  class="rounded-borders col-12 full-height" src="~assets/banner_1.jpg"  />
                 </div>
             </q-carousel-slide>
             <q-carousel-slide :name="2" class="column no-wrap">
                 <div class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap">
-                    <q-img class="rounded-borders col-12 full-height"  src="https://cdn.quasar.dev/img/donuts.png"  /> 
+                    <q-img  class="rounded-borders col-12 full-height" src="~assets/banner_1.jpg"  />
                 </div>
             </q-carousel-slide>
         </q-carousel>
